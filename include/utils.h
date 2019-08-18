8831625fe3879b7b504b2cbd4f583b2c3d023152
Hasan Ali <hasali20@outlook.com>
2019-08-18 19:40:43 +0000

    Add some timing util functions

--- include/utils.h
+++ include/utils.h
@@ -6,3 +6,18 @@
  * Waits for the specified number of cycles.
  */
 void wait_cycles(uint64_t cycles);
+
+/**
+ * Gets the frequency of the system counter (cntfrq_el0).
+ */
+uint64_t get_sys_counter_frequency(void);
+
+/**
+ * Gets the physical count value of the system counter (cntpct_el0).
+ */
+uint64_t get_sys_counter_count(void);
+
+/**
+ * Gets the value of the system counter (cntpct_el0) in milliseconds.
+ */
+uint64_t get_sys_counter_ms(void);
